c71558236b880cee0d3f88a72d35adfd6cd57d6c
Chien Quang Hoang <hoangchien090508@yahoo.com>
2021-04-01 16:55:25 +0700

    change path for web projects

--- src/web/dotnet/Program.cs
+++ src/web/dotnet/Program.cs
@@ -6,7 +6,7 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
+            Console.WriteLine("hello world!");  
             
         }
     }
